87d6f364926eb6c6fd4afd3d9db8d3ffccc6e999
Patcheresu <patcheresu@gmail.com>
2023-06-11 10:54:24 -0400

    Edited Enemy Collision, Keys can now be randomly placed

--- Scripts/Level/LockedExitLevelScript.gd
+++ Scripts/Level/LockedExitLevelScript.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 export (NodePath) onready var exit = get_node("Objects/KeyLockedExit")
 
+signal key_randomPos
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	emit_signal("key_randomPos")
 	exit.connect("leaving_level", self, "finish_level")
 
 func finish_level():
