a8a3b38b79bce46a42f4418a73eb14aca87ded25
Santi-A <santi.alvarez.iii@gmail.com>
2023-09-03 09:06:07 +0800

    fixed conflicts with pause menu and cutscene controller

--- Scripts/Menu/PauseMenu.gd
+++ Scripts/Menu/PauseMenu.gd
@@ -4,9 +4,20 @@ onready var options_menu = $OptionsMenu
 
 var is_paused = false setget set_is_paused
 
+var cutscene_control
+var cutscene_playing = false
+
+func _ready() -> void:
+	var tree = get_tree()
+	if tree.has_group("CutsceneControl"):
+		cutscene_control = tree.get_nodes_in_group("CutsceneControl")[0]
+	else:
+		print("No Cutscene Controller Found")
+
 func _unhandled_input(event):
 	if event.is_action_pressed("Escape"):
-		self.is_paused = !is_paused
+		handlePauseEvent()
+
 
 func set_is_paused(value):
 	is_paused = value
@@ -14,7 +25,7 @@ func set_is_paused(value):
 	visible = is_paused
 
 func _on_ResumeBtn_pressed():
-	self.is_paused = false
+	handlePauseEvent()
 
 
 func _on_OptionsBtn_pressed():
@@ -23,3 +34,15 @@ func _on_OptionsBtn_pressed():
 
 func _on_QuitBtn_pressed():
 	get_tree().quit()
+
+func handlePauseEvent():
+	if cutscene_control:
+		cutscene_playing = cutscene_control.cutscene_playing
+	else:
+		cutscene_playing = false
+	
+	if cutscene_playing:
+		cutscene_control.is_active = !cutscene_control.is_active
+		visible = !cutscene_control.is_active
+	else:
+		self.is_paused = !is_paused
