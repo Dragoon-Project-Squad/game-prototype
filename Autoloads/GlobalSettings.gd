4149807e05557b3031dead0bd21c26d86e15ce31
Jono <jonathontimm@hotmail.com>
2023-12-17 15:01:52 +0800

    Added more settings and a setting save file

--- Autoloads/GlobalSettings.gd
+++ Autoloads/GlobalSettings.gd
@@ -3,32 +3,107 @@ extends Node
 signal fps_displayed(value)
 signal mouse_sens_updated(value)
 
-func toggle_fullscreen(value):
-	get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (value) else Window.MODE_WINDOWED
+const SAVEFILE = "user://SAVEFILE.save"
 
+var appdata_path = ""
+var game_data = {}
+
+func _ready() -> void:
+	appdata_path = ProjectSettings.globalize_path("user://")
+	load_data()
+
+func load_data():
+	if FileAccess.file_exists(SAVEFILE):
+		var save_file = FileAccess.open(SAVEFILE, FileAccess.READ)
+		game_data = save_file.get_var()
+		save_file.close()
+	else:
+		game_data = {
+			"fullscreen_mode": 0,
+			"res_index": 7,
+			"vsync_mode": true,
+			"max_fps": 120.0,
+			"fps_visible": false,
+			"master_vol": 0.8,
+			"master_mute": false,
+			"sfx_vol": 1.0,
+			"sfx_mute": false,
+			"music_vol": 1.0,
+			"music_mute": false,
+			"mouse_sens": 0.1
+		}
+		save_data()
+
+
+func save_data():
+	var save_file = FileAccess.open(SAVEFILE, FileAccess.WRITE)
+	save_file.store_var(game_data)
+	save_file.close()
+
+
+# Video settings
+# Bug: fullscreen modes have a 1px outline?
+func toggle_fullscreen(index):
+	match  index:
+		0: # Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		1: # Window Mode
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		2: # Borderless Window
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+		3: # Borderless Fullscreen
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+	game_data.fullscreen_mode = index
+	save_data()
+
+func update_resolution(index):
+	game_data.res_index = index
+	save_data()
 
 func toggle_sync(value):
 	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED if (value) else DisplayServer.VSYNC_DISABLED)
+	game_data.vsync_mode = value
+	save_data()
 
 
 func toggle_fps_display(value):
 	emit_signal("fps_displayed", value)
+	game_data.fps_visible = value
+	save_data()
 
 
-#TODO: Is this the proper way to code this
+# TODO: Is this the proper way to code this?
+# Jono: probably, docs say this is the right value
 func set_max_fps(value):
 	Engine.max_fps = value if value < 500 else 0
+	game_data.max_fps = value
+	save_data()
 
-#TODO: Audio buses still need to be added
+
+# Audio
+# TODO: Still need sound effects, music to test with
 func update_master_vol(vol):
-	AudioServer.set_bus_volume_db(0, vol)
+	AudioServer.set_bus_volume_db(0, linear_to_db(vol))
+	game_data.master_vol = vol
+	save_data()
 
 func update_music_vol(vol):
-	AudioServer.set_bus_volume_db(1, vol)
+	AudioServer.set_bus_volume_db(1, linear_to_db(vol))
+	game_data.music_vol = vol
+	save_data()
 
 func update_sfx_vol(vol):
-	AudioServer.set_bus_volume_db(2, vol)
+	AudioServer.set_bus_volume_db(2, linear_to_db(vol))
+	game_data.sfx_vol = vol
+	save_data()
 
 
+# Gameplay
 func update_mouse_sens(value):
 	emit_signal("mouse_sens_updated", value)
+	game_data.mouse_sens = value
+	save_data()
--- Scripts/Menu/OptionsMenu.gd
+++ Scripts/Menu/OptionsMenu.gd
@@ -1,7 +1,11 @@
 extends Control
 
+# Read game_data dictionary
+@onready var game_data = GlobalSettings.game_data
+
 # Video Settings
-@onready var display_options = $SettingsTab/Video/MarginContainer/VideoSettings/DisplayOptionBtn
+@onready var display_options = $SettingsTab/Video/MarginContainer/VideoSettings/ScreenModeBtn
+@onready var res_opts = $SettingsTab/Video/MarginContainer/VideoSettings/ResBtn
 @onready var vsync_btn = $SettingsTab/Video/MarginContainer/VideoSettings/VsyncBtn
 @onready var display_fps_btn = $SettingsTab/Video/MarginContainer/VideoSettings/DisplayFpsBtn
 @onready var max_fps_val = $SettingsTab/Video/MarginContainer/VideoSettings/HBoxContainer/MaxFpsVal
@@ -9,20 +13,56 @@ extends Control
 
 # Audio Settings
 @onready var master_slider = $SettingsTab/Audio/MarginContainer2/AudioSettings/MasterVolSlider
+@onready var master_vol_txt = $SettingsTab/Audio/MarginContainer2/AudioSettings/MasterVolLabel
+@onready var master_mute = false
 @onready var music_slider = $SettingsTab/Audio/MarginContainer2/AudioSettings/MusicVolSlider
+@onready var music_vol_txt = $SettingsTab/Audio/MarginContainer2/AudioSettings/MusicVolLabel
+@onready var music_mute = false
 @onready var sfx_slider = $SettingsTab/Audio/MarginContainer2/AudioSettings/SfxVolSlider
+@onready var sfx_vol_txt = $SettingsTab/Audio/MarginContainer2/AudioSettings/SfxVolLabel
+@onready var sfx_mute = false
 
 # Gameplay Settings
 @onready var mouse_sens_amount = $SettingsTab/Gameplay/MarginContainer3/GameplaySettings/MouseSensOption/MouseSensAmount
 @onready var mouse_slider = $SettingsTab/Gameplay/MarginContainer3/GameplaySettings/MouseSensOption/MouseSensSlider
 
 
-func _ready():
-	pass
+# Testing resolution list
+var resolutions: Dictionary = {
+	"3840x2160":Vector2i(3840,2160),
+	"2560x1440":Vector2i(2560,1440),
+	"1920x1080":Vector2i(1920,1080),
+	"1600x900":Vector2i(1600,900),
+	"1440x900":Vector2i(1440,900),
+	"1536x864":Vector2i(1536,864),
+	"1366x768":Vector2i(1366,768),
+	"1280x720":Vector2i(1280,720),
+	"1024x600":Vector2i(1024,600),
+	"800x600":Vector2i(800,600)
+	}
+
+func _ready() -> void:
+	add_resolutions()
+	display_options.selected = game_data.fullscreen_mode
+	max_fps_slider.value = game_data.max_fps
+	master_slider.value = game_data.master_vol
+	music_slider.value = game_data.music_vol
+	sfx_slider.value = game_data.sfx_vol
+	mouse_slider.value = game_data.mouse_sens
+
+func add_resolutions():
+	var CurrentResolution = get_viewport().get_size()
+	var Index = -1
+
+	for r in resolutions:
+		res_opts.add_item(r,Index)
+		if resolutions[r] == CurrentResolution:
+			res_opts._select_int(Index)
+		Index += 1
 
 
 func _on_DisplayOptionBtn_item_selected(index):
-	GlobalSettings.toggle_fullscreen(true if index == 1 else false)
+	GlobalSettings.toggle_fullscreen(index)
 
 
 func _on_VsyncBtn_toggled(button_pressed):
@@ -40,14 +80,20 @@ func _on_MaxFpsSlider_value_changed(value):
 
 func _on_MasterVolSlider_value_changed(value):
 	GlobalSettings.update_master_vol(value)
+	var percent = value * 100
+	master_vol_txt.text = ("Master volume %.0f" % percent)
 
 
 func _on_MusicVolSlider_value_changed(value):
 	GlobalSettings.update_music_vol(value)
+	var percent = value * 100
+	music_vol_txt.text = ("Music volume %.0f" % percent)
 
 
 func _on_SfxVolSlider_value_changed(value):
 	GlobalSettings.update_sfx_vol(value)
+	var percent = value * 100
+	sfx_vol_txt.text = ("SFX volume %.0f" % percent)
 
 #TODO: Need to fix functionality
 func _on_MouseSensSlider_value_changed(value):
@@ -57,3 +103,8 @@ func _on_MouseSensSlider_value_changed(value):
 
 func _on_close_btn_pressed() -> void:
 	visible = false
+
+
+func _on_res_btn_item_selected(index: int) -> void:
+	GlobalSettings.update_resolution(resolutions.values()[index])
+	DisplayServer.window_set_size(resolutions.values()[index])
