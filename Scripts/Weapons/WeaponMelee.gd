adbc1f5e48bbd1b927a886f50157435b3bd411be
Santi-A <santi.alvarez.iii@gmail.com>
2024-01-28 12:14:21 +0800

    applied weapon changes to melee weapon script

--- Scripts/Weapons/WeaponMelee.gd
+++ Scripts/Weapons/WeaponMelee.gd
@@ -7,20 +7,29 @@ var hitboxActive : bool = false
 @export var weaponOrigin : Node
 @export var animationPlayer : Node
 
-@export var weaponOriginStartScale: Vector2 
+@export var weaponId : String
+var weaponData = null
+var bulletCD = null
 
-func _ready():
-	initWeapon(weaponResource)
-	weaponHitbox.body_entered.connect(bodyEnteredHitbox)
-	weaponHitbox.monitoring = false
-
-func _process(_delta):
-	if Input.is_action_pressed("Shoot") or isAttackBuffered:
-		var isBulletShot = attemptAttack()
+#Shooting
+var isAttackBuffered := false
+var timeLastAttacked: float
+const ATTACK_BUFFER_PERIOD := 0.2
 
 #Aesthetics
 @export var WEAPON_ROTATE_LERP_CONSTANT := 0.1
 var shootDirection: Vector2
+
+func _ready():
+	if WeaponData.weapon_ids.has(weaponId):
+		for weapon in WeaponData.weapons:
+			if weapon.weapon_id == weaponId:
+				initWeapon(weapon)
+	weaponHitbox.body_entered.connect(bodyEnteredHitbox)
+	weaponHitbox.monitoring = false
+
+func _process(_delta):
+	handleInput()
 
 func pointToMouse():
 	shootDirection = (get_global_mouse_position() - global_position).normalized()
@@ -31,25 +40,19 @@ func pointToMouse():
 	else:
 		weaponOrigin.get_child(0).flip_v = false
 
-#Shooting
-@export var weaponResource: Resource
-
-var isAttackBuffered := false
-var timeLastAttacked: float
-const ATTACK_BUFFER_PERIOD := 0.2
-
-
-func initWeapon(_weaponResource: WeaponResource):
-	timeLastAttacked = -_weaponResource.BULLET_CD_PERIOD 
+func initWeapon(weapon):
+	weaponData = weapon
+	bulletCD = 1 / weaponData.bullets_per_second
+	timeLastAttacked = bulletCD
 
 #Returns if bullet shoot successful
 func attemptAttack() -> bool:
 	var currentTime = Time.get_ticks_usec() / 1000000.0
 	
-	if currentTime - timeLastAttacked > weaponResource.BULLET_CD_PERIOD - ATTACK_BUFFER_PERIOD:
+	if currentTime - timeLastAttacked > bulletCD - ATTACK_BUFFER_PERIOD:
 		isAttackBuffered = true
 	
-	if currentTime - timeLastAttacked < weaponResource.BULLET_CD_PERIOD:
+	if currentTime - timeLastAttacked < bulletCD:
 		return false
 	
 	timeLastAttacked = currentTime
@@ -69,3 +72,12 @@ func enableHitbox():
 
 func disableHitbox():
 	weaponHitbox.monitoring = false
+
+func handleInput():
+	if weaponData:
+		if weaponData.firing_mode == "single":
+			if Input.is_action_just_pressed("Shoot") or isAttackBuffered:
+				var isAttacking = attemptAttack()
+		if weaponData.firing_mode == "auto":
+			if Input.is_action_pressed("Shoot") or isAttackBuffered:
+				var isAttacking = attemptAttack()
